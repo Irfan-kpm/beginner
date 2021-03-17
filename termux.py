06c527d48023eb724664a369a1f30614b4b8cbee
Irfan <77621100+Irfan-kpm@users.noreply.github.com>
2021-03-17 19:00:13 +0530

    Add files via upload

---
+++ termux.py
@@ -0,0 +1,63 @@
+import os
+import time
+import pyfiglet
+print("+-----------------------------------------------+")
+print("        -------[ @Irfan_kpm ]---------")
+print("[#] This tool was made for termux beginners")
+print("+-----------------------------------------------+")
+time.sleep(2)
+os.system("clear")
+#####
+print("---apt update----")
+time.sleep(2)
+os.system("apt update -y")
+time.sleep(1)
+os.system("clear")
+####
+print("---apt upgrade---")
+time.sleep(2)
+os.system("apt upgrade -y")
+time.sleep(1)
+os.system("clear")
+#####
+print("--setup storage--")
+time.sleep(2)
+os.system("termux-setup-storage -y")
+time.sleep(1)
+os.system("clear")
+####
+print("---python3--")
+time.sleep(2)
+os.system("pkg install python3 -y")
+time.sleep(1)
+os.system("clear")
+####
+print("---upgrade pip---")
+time.sleep(2)
+os.system("pip install --upgrade pip")
+time.sleep(1)
+os.system("clear")
+####
+print("----pyfiglet---")
+time.sleep(2)
+os.system("pip install pyfiglet")
+time.sleep(2)
+os.system("clear")
+####
+print("--Nmap---")
+time.sleep(2)
+os.system("pkg install nmap -y")
+time.sleep(1)
+os.system("clear")
+####
+print("---php---")
+time.sleep(2)
+os.system("pkg install php -y")
+time.sleep(1)
+os.system("clear")
+time.sleep(2)
+####
+print("------END------")
+
+
+
